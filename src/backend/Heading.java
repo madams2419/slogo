21dfd06495a9abb91c941f95019c8baafaa4bd44
Michael Adams <mba13@duke.edu>
2015-02-21 15:47:49 -0500

    refactor grid, line, heading, and turtle classes.

--- src/backend/Heading.java
+++ src/backend/Heading.java
@@ -4,22 +4,22 @@ import java.lang.Math;
 
 public class Heading {
 
-  private double angle;
+	private double angle;
 
-  public Heading(double angle) {
-    this.angle = angle;
-  }
+	public Heading(double angle) {
+		this.angle = angle;
+	}
 
-  public void setAngle(double newAngle) {
-    angle = newAngle;
-  }
+	public void setAngle(double newAngle) {
+		angle = newAngle;
+	}
 
-  public double getAngle() {
-    return angle;
-  }
+	public double getAngle() {
+		return angle;
+	}
 
-  public double getAngleRads() {
-    return (angle / 360) * Math.PI;
-  }
+	public double getAngleRads() {
+		return (angle / 180) * Math.PI;
+	}
 
 }
--- src/backend/Turtle.java
+++ src/backend/Turtle.java
@@ -2,48 +2,61 @@ package backend;
 
 import java.lang.Math;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Turtle {
 
-  private Image image;
-  private Point location;
-  private Heading heading;
-  private Color penColor;
+	private Image image;
+	private Point location;
+	private Heading heading;
+	private Color penColor;
+	private ArrayList<Line> lines;
 
-  public Turtle(Image image, Point location, Heading heading, Color penColor) {
-    this.image = image;
-    this.location = location;
-    this.heading = heading;
-    this.penColor = penColor;
-  }
+	public Turtle(Image image, Point location, Heading heading, Color penColor) {
+		this.image = image;
+		this.location = location;
+		this.heading = heading;
+		this.penColor = penColor;
+		lines = new ArrayList<>();
+	}
 
-  protected void move(int magnitude) {
-    int newX = (int) Math.cos(heading.getAngleRads()) * magnitude;
-    int newY = (int) Math.sin(heading.getAngleRads()) * magnitude;
-    location = new Point(newX, newY);
-  }
+	protected void move(int magnitude) {
+		int newX = (int) Math.cos(heading.getAngleRads()) * magnitude;
+		int newY = (int) Math.sin(heading.getAngleRads()) * magnitude;
+		Point nextLocation = new Point(newX, newY);
 
-  protected void rotate(double degrees) {
-    heading.setAngle(heading.getAngle() + degrees);
-  }
+		Line newLine = new Line(location, nextLocation, penColor);
 
-  public void setImage(Image newImage) {
-    image = newImage;
-  }
+		location = nextLocation;
+		lines.add(newLine);
+	}
 
-  public void setPenColor(Color newPenColor) {
-    penColor = newPenColor;
-  }
+	protected void rotate(double degrees) {
+		heading.setAngle(heading.getAngle() + degrees);
+	}
 
-  public Image getImage() {
-    return image;
-  }
+	public void setImage(Image newImage) {
+		image = newImage;
+	}
 
-  public Point getLocation() {
-    return location;
-  }
+	public void setPenColor(Color newPenColor) {
+		penColor = newPenColor;
+	}
 
-  public Heading getHeading() {
-    return heading;
-  }
+	public Image getImage() {
+		return image;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+
+	public Heading getHeading() {
+		return heading;
+	}
+
+	public ArrayList<Line> getLines() {
+		return lines;
+	}
+
 }
