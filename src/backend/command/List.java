b6707c951435c464b969950fb7dba92a2abfd5eb
Michael Adams <mba13@duke.edu>
2015-02-28 15:50:39 -0500

    add List get method.

--- src/backend/command/List.java
+++ src/backend/command/List.java
@@ -20,6 +20,10 @@ public class List extends Command {
 		listComplete = true;
 	}
 
+	public Command get(int index) {
+		return params.get(index);
+	}
+
 	public Double execute() {
 		Double returnVal = 0.0;
 		for(Command param : params) {
