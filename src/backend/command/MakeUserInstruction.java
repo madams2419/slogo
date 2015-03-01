8a826d3ea6f493fd30a63f2957ca4bc44b081162
Michael Adams <mba13@duke.edu>
2015-02-28 22:34:51 -0500

    initial MakeUserInstruction implementation (nonfunctional).

---
+++ src/backend/command/MakeUserInstruction.java
@@ -0,0 +1,32 @@
+package backend.command;
+
+import java.util.HashMap;
+
+import backend.StringPair;
+
+public class MakeUserInstruction extends Command {
+
+	HashMap<String, Double> userVariables;
+	HashMap<String, Command> userInstructions;
+
+	public MakeUserInstruction(StringPair stringPair, HashMap<String, Command> userInstructions, HashMap<String, Double> userVariables, Command parent) {
+		super(stringPair, 3, parent);
+		this.userInstructions = userInstructions;
+		this.userVariables = userVariables;
+	}
+
+	public Double execute() {
+		String instName = getParam(0).getValue();
+		List paramNames = (List) getParam(1);
+		List commandList = (List) getParam(2);
+		StringPair newSP = new StringPair("Command", instName);
+
+		InstructionDefinition newInstDef = new InstructionDefinition(newSP, paramNames, commandList, userVariables, null);
+
+		// add instruction definition to model list
+		userInstructions.put(instName, newInstDef);
+
+		return 1.0;
+	}
+
+}
